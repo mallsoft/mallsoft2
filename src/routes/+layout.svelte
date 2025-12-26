af4495b664f5ad562f18bfe2a3170503e3b83fcf
mallsoft <36839009+mallsoft@users.noreply.github.com>
2025-12-27 00:11:35 +0100

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-	import './global.css';
 	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
+	<link rel="preload" as="style" href="./global.css" />
 	<link rel="icon" href={favicon} />
 	<title>mallsoft.dev</title>
 	<meta
@@ -15,3 +15,23 @@
 </svelte:head>
 
 {@render children()}
+
+<style>
+	:global(:root) {
+		color: #4929e6;
+		font-weight: normal;
+		font-size: x-large;
+		font-family: Verdana, sans-serif;
+		line-height: 1.7;
+	}
+
+	:global(body) {
+		margin: 0;
+	}
+
+	@media (width < 600px) {
+		:global(:root) {
+			font-size: large;
+		}
+	}
+</style>
