60aa894c4d12a92bfc2a0965f19920e7b694140a
mallsoft <36839009+mallsoft@users.noreply.github.com>
2025-12-29 11:16:02 +0100

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,12 @@
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: [vitePreprocess(), mdsvex()],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({})
 	},
 	compilerOptions: { runes: true },
 	extensions: ['.svelte', '.svx']
